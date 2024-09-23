124c60ba16d4075bce4ff34ade004db271af495d
/c <ooodenieska123@gmail.com>
2024-09-24 00:01:28 +0300

    24/09-1

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-name = "Denis"
-print ("Hello" + name)
+name = "example"
+print ( name [0])
+print ( name [-1])
+print ('urban' [2:5])
+print ('urban' [::-1])
+print ('Топинамбур' [1::2] )
+print ('Топинамбур'[0])
+print ('Топинамбур'[-1])
+print ('Топинамбур'[5:9])
+print ('Топинамбур'[::-1])
+print ('Топинамбур'[1::2])

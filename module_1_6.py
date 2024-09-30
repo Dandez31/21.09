03deb3be88c829d9e9169f44119496c249ad3911
/c <ooodenieska123@gmail.com>
2024-09-30 17:02:19 +0300

    1234567

--- module_1_6.py
+++ module_1_6.py
@@ -32,15 +32,16 @@ print("Dict: " + str(my_dict) )
 print("Existing value: " + str(my_dict["Richard"]))
 print("Not existing value: " + str(my_dict.get("Kamila")))
 print("Deleted value: " + str(my_dict.pop("Smit")))
-print("Modified dictionary: " + str(my_dict.update({"Vova":1994,"Serg":1995})))
-print(my_dict)
+my_dict.update({"Kamila": 1994, "Serg": 1993})
+print("Modified dictionary: " + str(my_dict) + "\n")
 
 my_set = {1, "Pineapple", 3.14,}
 print("Set: " + str(my_set))
 my_set.add(5)
-my_set.add(10)
-my_set.add(4.5)
-my_set.discard(1)
+my_set.add(111)
+my_set.add((1, 5, 6))
+my_set.discard(3.14)
+#my_set.remove(1)
 print("Modified set: " + str(my_set))
 
 
